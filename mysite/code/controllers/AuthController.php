d11ab7f6e7f7605820deac1bf90014bcc2c9ee5a
rchntrl <archantyrael@gmail.com>
2015-02-03 16:51:28 +0600

    auth

--- mysite/code/controllers/AuthController.php
+++ mysite/code/controllers/AuthController.php
@@ -47,9 +47,9 @@ class AuthController extends Controller {
     public function test() {
         $params = array(
             'client_id'     => $this->config['client_id'],
-            'redirect_uri'  => $this->config['redirect_uri'],
+            'redirect_uri'  => urldecode($this->config['redirect_uri']),
             'response_type' => 'code',
-            //'auth_flow'     => 'auth_code',
+            'auth_flow'     => 'auth_code',
             'scope'         => 'wow.profile+sc2.profile'
         );
         /*
