b67b19d9d08d34aadc580746b1dd3686280c2be4
rchntrl <archantyrael@gmail.com>
2015-10-09 02:28:55 +0600

    HOTS breadcrumbs

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -93,7 +93,7 @@ class Page extends SiteTree {
         $crumbs[] = array(
             'Title' => SiteConfig::current_site_config()->Title,
             'Link' => Director::absoluteBaseURL(),
-            'Self' => false,
+            'Self' => true,
         );
         return array_reverse($crumbs);
     }
